d84026ccba3ec5d5cfc0e8d1cf52562522cf6874
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-05-20 19:24:44 +0100

    Landing page setup

---
+++ src/types/generated.d.ts
@@ -0,0 +1,14 @@
+/*
+* This file is auto-generated and modified by Gots (https://github.com/aosasona/gots). 
+* DO NOT MODIFY THE CONTENT OF THIS FILE
+*/
+
+export type JoinWaitlistRequest = {
+	name: string;
+	email: string;
+}
+
+export type JoinWaitlistResponse = {
+	waitlist_id: number;
+}
+
