62be807168dc9d946a98bf564cf8638e30c0e8cb
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-09-13 16:29:01 +0100

    Update problem definition - rough draft

--- src/lib/errors/index.ts
+++ src/lib/errors/index.ts
@@ -1,27 +1,103 @@
+class UnwrapError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "UnwrapError";
+  }
+}
+
 type Nullable<T> = T | null;
 type NullableObject = Nullable<Record<string, any>>;
 
-type ErrorType = "validation" | "other";
+type ErrorType = "single" | "multiple" | "none";
 
-type Problem<P extends ErrorType, I extends NullableObject> = P extends "other" ? string : P extends "validation" ? { message?: string; errors: Record<keyof I, string> } : never;
+type BaseProblem<E extends ErrorType> = { type: E; error: () => string };
 
-type ErrResult<Input extends NullableObject> = { ok: false; problem: Problem<ErrorType, Input>; code?: number; error: () => string };
+type Problem<E extends ErrorType, I extends NullableObject> = E extends "single"
+  ? { message: string }
+  : E extends "multiple"
+  ? { message?: string; errors: Record<keyof I, string> }
+  : never;
+
+type ErrInput<E extends ErrorType, I extends NullableObject> = E extends "single" ? string : { message?: string; errors: Record<keyof I, string> };
+
+type ErrResult<Input extends NullableObject> = BaseProblem<ErrorType> & {
+  ok: false;
+  problem: Problem<ErrorType, Input>;
+  code: number;
+};
 
 type OkResult<Response extends NullableObject> = { ok: true; message: string; data: Response };
 
 type Result<Response extends NullableObject, Input extends NullableObject> = OkResult<Response> | ErrResult<Input>;
 
-function Ok<T extends NullableObject>(message: string, data: T = null as T): Result<T, null> {
-  return { ok: true, message, data };
+function Ok<T extends NullableObject, I extends NullableObject>(message?: string, data: T = null as T): Result<T, I> {
+  return { ok: true, message: message ?? "OK", data };
 }
 
-function Err<T extends object, I extends object, E extends ErrorType>(problem: Problem<E, I>, code: number = 500): Result<T, I> {
-  return { ok: false, problem, code, error: () => (typeof problem === "string" ? problem : problem.message ?? "Something went wrong") };
+function Err<T extends NullableObject, I extends NullableObject, E extends ErrorType>(problem: ErrInput<E, I>, code: number = 500): Result<T, I> {
+  if (typeof problem === "string") {
+    return {
+      ok: false,
+      code,
+      type: "single",
+      problem: { message: problem },
+      error() {
+        return this.problem?.message ?? "Something went wrong!";
+      },
+    };
+  }
+
+  return {
+    ok: false,
+    type: "multiple",
+    problem: {
+      ...problem,
+    },
+    code,
+    error() {
+      return this.problem?.message ?? "One or more errors occurred!";
+    },
+  };
 }
+
+/////
+
+// TODO: get the exact error type and pass it to the match cases so that problem is typed correctly
+type MatchCases<T extends NullableObject, I extends NullableObject, R> = {
+  ok: (data: T) => R;
+  err: (problem: Problem<ErrorType, I>) => R;
+};
 
 function isOk<T extends NullableObject, I extends NullableObject>(result: Result<T, I>): result is OkResult<T> {
   return result.ok;
 }
 
-export type { Result, NullableObject };
-export { Ok, Err, isOk };
+function isErr<T extends NullableObject, I extends NullableObject>(result: Result<T, I>): result is ErrResult<I> {
+  return !result.ok;
+}
+
+function match<T extends NullableObject, I extends NullableObject, R>(result: Result<T, I>, cases: MatchCases<T, I, R>): R {
+  return result.ok ? cases.ok(result.data) : cases.err(result.problem);
+}
+
+function when<T extends NullableObject, I extends NullableObject>(result: Result<T, I>, cases: MatchCases<T, I, void>): void {
+  match(result, cases);
+}
+
+/**
+ * Unwraps the result, returning the data if it's Ok, or throwing an error if it's Err.
+ *
+ * @param result The result to unwrap.
+ * @returns The data if the result is Ok.
+ * @throws {UnwrapError} if the result is Err.
+ */
+
+function unwrap<T extends NullableObject, I extends NullableObject>(result: Result<T, I>): T | null {
+  if (!result.ok) {
+    throw new UnwrapError(result.error());
+  }
+  return result.data;
+}
+
+export type { Result, Nullable, NullableObject };
+export { Ok, Err, match, when, isOk, isErr, unwrap, UnwrapError };
--- src/lib/requests/index.ts
+++ src/lib/requests/index.ts
@@ -1,7 +1,17 @@
 import apisauce from "apisauce";
-import type { NullableObject, Result } from "../errors";
+import { Err, Ok, type NullableObject, type Result } from "../errors";
 
 type Endpoint = `/${string}`;
+
+type CustomApiResponse<T extends NullableObject, M extends NullableObject = null> = {
+  ok: boolean;
+  message: string;
+  status_code: number;
+  data: T;
+  error?: string;
+  errors?: any;
+  meta?: M;
+};
 
 const API_URL = (import.meta.env.VITE_API_URL as string) ?? "https://api.sidekyk.app/api/v1";
 
@@ -9,6 +19,16 @@ const api = apisauce.create({
   baseURL: API_URL,
 });
 
-async function post<R extends NullableObject = null, I extends NullableObject = null>(endpoint: Endpoint, data: I): Promise<Result<R, I>> { }
+async function post<Input extends NullableObject, Response extends NullableObject>(endpoint: Endpoint, data: Input): Promise<Result<Response, Input>> {
+  try {
+    const response = await api.post<CustomApiResponse<Response>>(endpoint, data);
+    if (!response.ok) {
+    }
+    return Ok(response?.data?.message, response?.data?.data);
+  } catch (error) {
+    return Err((error as Error)?.message ?? "Something went wrong");
+  }
+}
 
+export type { CustomApiResponse };
 export { api };
